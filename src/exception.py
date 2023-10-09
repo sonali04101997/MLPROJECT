08f1e48d6d7e231638a59fa9f398d5b19912d7cc
sonali04101997 <sonalinale884@gmail.com>
2023-10-09 21:18:26 +0530

    EDA & problem statements

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,8 @@
 import sys
+sys.path.append('E:\projects\mlproject$')
+
 from src.logger import logging
+
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -9,8 +12,6 @@ def error_message_detail(error,error_detail:sys):
 
     return error_message
 
-    
-
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
@@ -19,3 +20,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+    
+    if __name__=='__main__':
+        
+        try:
+            a=1/0
+        except Exception as e:
+            logging.info('divede by zero')
+            raise CustomException(e,sys)
